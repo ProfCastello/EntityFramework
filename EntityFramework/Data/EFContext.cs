f4ce2d70aeef919d5d486f58d0a5fbae85b32439
Prof-Castello <gustavo.castello@docente.senai.br>
2024-02-02 16:06:46 -0300

    Criando migration de cliente

---
+++ EntityFramework/Data/EFContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EntityFramework.Data
+{
+    public class EFContext : DbContext
+    {
+        public EFContext(DbContextOptions<EFContext> options) : base(options) { }
+
+        public DbSet<Models.Cliente> Clientes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Models.Cliente>().ToTable("Cliente");
+        }
+    }
+}
--- EntityFramework/Program.cs
+++ EntityFramework/Program.cs
@@ -1,7 +1,14 @@
+using EntityFramework.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("DbConnection");
+
+builder.Services.AddDbContext<EFContext>(options => options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
